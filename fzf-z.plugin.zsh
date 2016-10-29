d6f9f2dc3d071a4c58b724ab90528ea6c4fa990c
Andrew Ferrier <andrewferrier@example.com>
2016-10-29 14:27:12 +0100

    Handle reversing logic differently depending on OS platform.

--- fzf-z.plugin.zsh
+++ fzf-z.plugin.zsh
@@ -3,13 +3,19 @@
 # Based on https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh
 # (MIT licensed, as of 2016-05-05).
 
+if [[ $OSTYPE == darwin* ]]; then
+    REVERSER='tail -r'
+else
+    REVERSER='tac'
+fi
+
 __fzfz() {
     if (($+FZFZ_EXTRA_DIRS)); then
         FZFZ_EXTRA_DIRS="{ find $FZFZ_EXTRA_DIRS -type d 2> /dev/null }"
     else
         FZFZ_EXTRA_DIRS="{ true }"
     fi
-    local cmd="{ { z -l | tail -r | sed 's/^[[:digit:].]*[[:space:]]*//' }; $FZFZ_EXTRA_DIRS; }"
+    local cmd="{ { z -l | $REVERSER | sed 's/^[[:digit:].]*[[:space:]]*//' }; $FZFZ_EXTRA_DIRS; }"
     eval "$cmd" | fzf --tiebreak=index -m --preview="ls -1 {} | head -$LINES" | while read item; do
     printf '%q ' "$item"
   done
